daf0281462040c8f7f8156756b3baf7eb95c0f58
LazariTodorov <litodorov10@gmail.com>
2023-10-26 15:45:08 +0300

    Basic parts

---
+++ src/com/company/ComicBook.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class ComicBook extends Book{
+
+    private String characterName;
+
+    public String getCharacterName() {
+        return characterName;
+    }
+
+    public void setCharacterName(String characterName) {
+        this.characterName = characterName;
+    }
+}
